79d3da0ad957bd21cff7b305bdeb40cc04a38a67
Ahmet Mert Ozdemir <avamertozdemir@gmail.com>
2022-06-03 20:56:39 +0300

    data added and showed on local

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Itemcard from "./Itemcard";
+import data from "./data";
 function Home() {
   return (
     <>
-      <h1 className="text-center mt-3">All Items</h1>
-      <section className="py-4 container">
-        <div className="row justify-content-center">
-          <Itemcard img="" title="title" description="Description" />
-        </div>
-      </section>
+      <div>
+        <h1 className="text-center mt-3">All Items</h1>
+        <section className="py-4 container">
+          <div className="row justify-content-center">
+            {data.map((item) => {
+              return (
+                <Itemcard
+                  img={item.image}
+                  title={item.title}
+                  price={item.price}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </div>
     </>
   );
 }
--- src/Itemcard.js
+++ src/Itemcard.js
@@ -4,10 +4,10 @@ function Itemcard(props) {
   return (
     <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
       <div class="card p-0 overflow-hidden h-100 shadow">
-        <img src={props.img} class="card-img-top img-fluid" />
+        <img src={props.img} class="card-img-top img-fluid" alt="Product" />
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.description}</p>
+          <p class="card-text">{props.price}</p>
           <button class="btn btn-primary">Add to cart</button>
         </div>
       </div>
