64bf949fe56a374690b0905c143c11f0f5ea76ce
Ahmet Mert Ozdemir <avamertozdemir@gmail.com>
2022-06-04 09:14:46 +0300

    Cart functionality is done

--- src/Cart.js
+++ src/Cart.js
@@ -11,7 +11,71 @@ function Cart() {
     removeItem,
     emptyCart,
   } = useCart();
-  return <div>{console.warn(items)}</div>;
+
+  if (isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>;
+
+  return (
+    <section className="py-4 container">
+      <div className="row justify-content-center">
+        <div>
+          <h5>
+            Cart ({totalUniqueItems}) total Items ({totalItems}){" "}
+          </h5>
+          <table className="table table-light table-hover m-0">
+            <tbody>
+              {items.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={item.image} style={{ height: "6rem" }} />
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.price}</td>
+                    <td>Quantity ({item.quantity})</td>
+                    <td>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity - 1)
+                        }
+                      >
+                        -
+                      </button>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity + 1)
+                        }
+                      >
+                        +
+                      </button>
+                      <button
+                        className="btn btn-danger ms-2"
+                        onClick={() => removeItem(item.id)}
+                      >
+                        Remove Item
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-auto ms-auto">
+          <h2>Total Price: ${cartTotal.toFixed(2)}</h2>
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-danger m-2" onClick={emptyCart}>
+            Clear Cart
+          </button>
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-info m-2">Buy Now</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Cart;
--- src/Itemcard.js
+++ src/Itemcard.js
@@ -5,12 +5,19 @@ function Itemcard(props) {
   const { addItem } = useCart();
   return (
     <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-      <div class="card p-0 overflow-hidden h-100 shadow text-center">
-        <img src={props.img} class="card-img-top img-fluid " alt="Product" />
-        <div class="card-body">
-          <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.price}</p>
-          <button class="btn btn-primary" onClick={() => addItem(props.item)}>
+      <div className="card p-0 overflow-hidden h-100 shadow text-center">
+        <img
+          src={props.img}
+          className="card-img-top img-fluid "
+          alt="Product"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.price}</p>
+          <button
+            className="btn btn-primary"
+            onClick={() => addItem(props.item)}
+          >
             Add to cart
           </button>
         </div>
